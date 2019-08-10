79e722d7809dd929a141145e8f73e18456356c80
Gubi <guneedmts@gmail.com>
2019-08-10 18:00:53 +1000

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+import { ListingsComponent } from './listings/listings.component';
+import { DetailComponent } from './detail/detail.component';
+// import {NgbButtonsModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    ListingsComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'listings', component: ListingsComponent},
+      {path: 'listings/:id', component: DetailComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]),
+    ProductModule,
+  ],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
